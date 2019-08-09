ab9182b1b4ea7145ddd36a37b16c0e28eed7d6e1
Juan Reis <juan.ribeiroreis@gmail.com>
2019-08-09 15:03:44 -0300

    Versão 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
